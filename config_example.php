b5b16b60a0c4ddff188693a74fa602f0b594abe5
Alexandr Zeinalov <zeinalov@nm.ru>
2012-10-04 12:50:27 +0400

    Add config, some code cleanup (attempt=2)

---
+++ config_example.php
@@ -0,0 +1,13 @@
+<?php
+
+$base = "http://test.ru/qreg";
+$name = "TEST HUB";
+$from = "test@test.ru";
+
+$db_host = "localhost";
+$db_data = "verlihubdata";
+$db_user = "verlihubuser";
+$db_pass = "verlihubpass";
+$db_charset = "cp1251";
+
+?>
